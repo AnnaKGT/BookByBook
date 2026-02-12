2df5a072d38b4e6bfe48f4659514c152ad72ec7c
Anna KGT <106539309+AnnaKGT@users.noreply.github.com>
2026-02-12 15:41:25 +0200

    s-9

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 
 import 'package:book_by_book/constants/routes.dart';
-import 'package:book_by_book/firebase_options.dart';
 import 'package:book_by_book/services/auth/auth_service.dart';
 import 'package:book_by_book/views/login_view.dart';
 import 'package:book_by_book/views/mainpage_view.dart';
