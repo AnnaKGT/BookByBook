0ec3652d39e2c83b734fd7448ca4e511601b697d
Anna KGT <106539309+AnnaKGT@users.noreply.github.com>
2026-02-23 13:19:17 +0200

    step-24

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,9 +1,9 @@
-
 import 'package:book_by_book/constants/routes.dart';
 import 'package:book_by_book/services/auth/auth_exceptions.dart';
 import 'package:book_by_book/services/auth/auth_service.dart';
 import 'package:book_by_book/services/auth/bloc/auth_bloc.dart';
 import 'package:book_by_book/services/auth/bloc/auth_event.dart';
+import 'package:book_by_book/services/auth/bloc/auth_state.dart';
 import 'package:book_by_book/utilities/dialogs/error_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -36,83 +36,82 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Log in'),
-        ),
+      appBar: AppBar(title: const Text('Log in')),
       body: FutureBuilder(
         future: AuthService.firebase().initialize(),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
-
             case ConnectionState.done:
               return Column(
-            children: [
-              TextField(
-                controller: _email,
-                keyboardType: TextInputType.emailAddress,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your email here'
-                ),
-              ),
-              TextField(
-                controller: _password,
-                obscureText: true,
-                enableSuggestions: false,
-                autocorrect: false,
-                decoration: const InputDecoration(
-                  hintText: 'Enter your password'
-                ),
-              ),
-              TextButton(
-                onPressed: () async {
+                children: [
+                  TextField(
+                    controller: _email,
+                    keyboardType: TextInputType.emailAddress,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your email here',
+                    ),
+                  ),
+                  TextField(
+                    controller: _password,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      hintText: 'Enter your password',
+                    ),
+                  ),
+                  BlocListener<AuthBloc, AuthState>(
+                    listener: (context, state) async {
+                      if (state is AuthStateLoggedOut) {
+                        if (state.exception is UserNotFoundAuthException || state.exception is WrongPasswordAuthException) {
+                          await showErrorDialog(context, "Invalid credentials");
+                        } else if (state.exception is InvalidCredentialsAuthException) {
+                          await showErrorDialog(context, "Invalid credentials");
+                        } else {
+                          await showErrorDialog(
+                            context,
+                            "Something went wrong. Try again.",
+                          );
+                        }
+                      }
+                    },
+                    child: TextButton(
+                      onPressed: () async {
+                        final email = _email.text.trim();
+                        final password = _password.text.trim();
 
-                  final email = _email.text.trim();
-                  final password = _password.text.trim();
-
-                  if (email.isEmpty || password.isEmpty) {
-                    await showErrorDialog(context, "Email and password cannot be empty");
-                    return;
-                  }
-                                
-                  try {
-                    context.read<AuthBloc>().add(
-                      AuthEventLogIn(
-                        email: email, 
-                        password: password,
-                        )
-                    );                    
-                  } on UserNotFoundAuthException {
-                    await showErrorDialog(context, "User not found");
-                  } on WrongPasswordAuthException {
-                    await showErrorDialog(context, "Wrong password");
-                  } on InvalidCredentialsAuthException {
-                    await showErrorDialog(context, "Invalid credentials");
-                  } on GenericAuthException {
-                     await showErrorDialog(context, "Something went wrong. Try again.");
-                  }
-                          
-                  }, child: const Text('Log in'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pushNamedAndRemoveUntil(
-                      registerRoute,
-                      (route) => false);
-                  }, 
-                  child: const Text("Not registered yet? Sign up here!"))
-            ],
-          );
-          default:
-          return const Text('Loading ...');      
+                        if (email.isEmpty || password.isEmpty) {
+                          await showErrorDialog(
+                            context,
+                            "Email and password cannot be empty",
+                          );
+                          return;
+                        }
+                         context.read<AuthBloc>().add(
+                            AuthEventLogIn(email: email, password: password),
+                          );
+                      },
+                      child: const Text('Log in'),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        registerRoute,
+                        (route) => false,
+                      );
+                    },
+                    child: const Text("Not registered yet? Sign up here!"),
+                  ),
+                ],
+              );
+            default:
+              return const Text('Loading ...');
           }
-          
         },
-        
       ),
     );
   }
-  
 }
-
